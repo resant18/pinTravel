f3cf95d55709358fb3d6992c4ca896071345083d
resant18 <santoso.renata@gmail.com>
2019-11-06 12:37:14 -0800

    Add more validation for session form

---
+++ app/assets/javascripts/validation.js
@@ -0,0 +1,14 @@
+export const isBlank = (input) => {
+    if (input === '') return true;
+    return false;
+}
+
+export const isValidInput = (input, type) => {
+    switch (type) {
+      case "email":
+        return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input));        
+      case "password":
+        return (input.length >= 6)
+    }
+    
+}
