0a87e41faa6805b7c15225324617747ec1f3809c
resant18 <santoso.renata@gmail.com>
2019-12-20 23:19:18 +0700

    Styling visibility checkbox in create board form

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -765,7 +765,7 @@ function (_React$Component) {
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
         d: "M15.18 12l7.16-7.16c.88-.88.88-2.3 0-3.18-.88-.88-2.3-.88-3.18 0L12 8.82 4.84 1.66c-.88-.88-2.3-.88-3.18 0-.88.88-.88 2.3 0 3.18L8.82 12l-7.16 7.16c-.88.88-.88 2.3 0 3.18.44.44 1.01.66 1.59.66.58 0 1.15-.22 1.59-.66L12 15.18l7.16 7.16c.44.44 1.01.66 1.59.66.58 0 1.15-.22 1.59-.66.88-.88.88-2.3 0-3.18L15.18 12z"
       })))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", {
-        "class": "borderline"
+        className: "borderline"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "body"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
@@ -777,18 +777,24 @@ function (_React$Component) {
         placeholder: "E.g. 'Places to go' or 'Recipes to make'",
         onChange: this.update('name')
       })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", {
-        "class": "borderline"
+        className: "borderline"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "board-visibility"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Visibility"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-        type: "checkbox"
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Visibility"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "secret"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+        className: "secret-box",
+        type: "checkbox",
+        value: "false"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "secret-info"
       }, "Keep this board secret."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://www.pinterest.com/_/_/help/article/change-board-privacy?source=secret_create"
-      }, "Learn more"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, "Learn more"))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "errors"
-      }, this.renderErrors()), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, this.renderErrors()), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", {
+        className: "borderline"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "button-footer"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "buttons-right"
--- frontend/components/board/board_form.jsx
+++ frontend/components/board/board_form.jsx
@@ -40,10 +40,10 @@ class BoardForm extends React.Component {
                         className='close-btn'
                         onClick={this.props.hideModal}
                     >
-                        <svg height="20" width="20" viewBox="0 0 24 24" aria-hidden="true" aria-label="" role="img"><path d="M15.18 12l7.16-7.16c.88-.88.88-2.3 0-3.18-.88-.88-2.3-.88-3.18 0L12 8.82 4.84 1.66c-.88-.88-2.3-.88-3.18 0-.88.88-.88 2.3 0 3.18L8.82 12l-7.16 7.16c-.88.88-.88 2.3 0 3.18.44.44 1.01.66 1.59.66.58 0 1.15-.22 1.59-.66L12 15.18l7.16 7.16c.44.44 1.01.66 1.59.66.58 0 1.15-.22 1.59-.66.88-.88.88-2.3 0-3.18L15.18 12z"></path></svg>
+                        <svg height='20' width='20' viewBox='0 0 24 24' aria-hidden='true' aria-label='' role='img'><path d='M15.18 12l7.16-7.16c.88-.88.88-2.3 0-3.18-.88-.88-2.3-.88-3.18 0L12 8.82 4.84 1.66c-.88-.88-2.3-.88-3.18 0-.88.88-.88 2.3 0 3.18L8.82 12l-7.16 7.16c-.88.88-.88 2.3 0 3.18.44.44 1.01.66 1.59.66.58 0 1.15-.22 1.59-.66L12 15.18l7.16 7.16c.44.44 1.01.66 1.59.66.58 0 1.15-.22 1.59-.66.88-.88.88-2.3 0-3.18L15.18 12z'></path></svg>
                     </button>
                 </div>
-                <hr class="borderline" />
+                <hr className='borderline' />
                 <div className='body'>
                     <form className='board-form'>
                         <div className='board-name'>
@@ -54,15 +54,20 @@ class BoardForm extends React.Component {
                                 onChange={this.update('name')}
                             />
                         </div>
-                        <hr class="borderline" />
+                        <hr className='borderline' />
                         <div className='board-visibility'>
                             <p>Visibility</p>
-                            <input type="checkbox" />
-                            <div className='secret'>Keep this board secret.</div>
-                            <a href="https://www.pinterest.com/_/_/help/article/change-board-privacy?source=secret_create">Learn more</a>
+                            <div className='secret'>
+                                <input className='secret-box' type='checkbox' value='false' />
+                                <div>
+                                    <div className='secret-info'>Keep this board secret.</div>
+                                    <a href='https://www.pinterest.com/_/_/help/article/change-board-privacy?source=secret_create'>Learn more</a>
+                                </div>
+                            </div>
                         </div>
                     </form>
-                    <div className="errors">{this.renderErrors()}</div>
+                    <div className='errors'>{this.renderErrors()}</div>
+                    <hr className='borderline' />
                     <div className='button-footer'>
                         <div className='buttons-right'>
                             <button
