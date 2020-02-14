81f0e18c7e47e28e307f13821646b46e3d2767e5
resant18 <santoso.renata@gmail.com>
2020-02-13 23:12:07 -0800

    Create dropdown component

---
+++ frontend/components/element/drop_down.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+// import '../../../app/assets/stylesheets/dropdown.scss';
+
+class DropDown extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         listOpen: false,
+         headerTitle: this.props.title
+      }
+
+      this.close = this.close.bind(this);
+   }
+
+   componentDidUpdate() {
+      const { listOpen } = this.state;
+
+      setTimeout(() => {
+         if (listOpen) {
+            window.addEventListener('click', this.close);
+         }
+         else {
+            window.removeEventListener('click', this.close);
+         }
+      }, 0);
+   }
+
+   componentWillUnmount() {
+      window.removeEventListener('click', this.close);
+   }
+
+   close(timeOut) {
+      this.setState({
+         listOpen: false
+      })
+   }
+
+   selectItem(title, id, stateKey) {
+      this.setState({
+         headerTitle: title,
+         listOpen: false
+      }, this.props.resetThenSet(id, stateKey));
+   }
+
+   toggleList() {
+      this.setState( prevState => ({
+         listOpen: !prevState.listOpen
+      }))
+   }
+
+   render() {
+      const { list } = this.props;
+      const { listOpen, headerTitle } = this.state;
+
+      return (
+         <div className="dd-wrapper">
+            <div className="dd-header" onClick={() => this.toggleList()}>
+               <div className="dd-header-title">{headerTitle}</div>
+               <svg height="12" width="12" viewBox="0 0 24 24" aria-label="Choose a board" role="img"><path d="M12 19.5L.66 8.29c-.88-.86-.88-2.27 0-3.14.88-.87 2.3-.87 3.18 0L12 13.21l8.16-8.06c.88-.87 2.3-.87 3.18 0 .88.87.88 2.28 0 3.14L12 19.5z"></path></svg>
+               {listOpen ? (
+                  ''
+               ) : (
+                  <button>Save</button>
+               )}
+            </div>
+            {listOpen && (
+               <ul className="dd-list" onClick={e => e.stopPropagation()}>
+                  {list.map(item => (
+                     <li
+                        className="dd-list-item"
+                        key={item.id}
+                        onClick={() =>
+                           this.selectItem(item.title, item.id, item.key)
+                        }
+                     >
+                        {item.title}{" "}
+                        {item.selected && 'Save'}
+                     </li>
+                  ))}
+               </ul>
+            )}
+         </div>
+      );
+   }
+}
+
+export default DropDown;
